7d01565a54871ba6875589127ceecb427eb1afae
Anthony Crisp <anthony_crisp@hotmail.com>
2017-04-26 13:39:20 +0100

    Getting test infrastructure set up

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  set :sessions, true
-
   get '/' do
-      'Hello Battle!'
+      'Testing infrastructure working!'
     end
+    run! if app_file == $0
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,18 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+ENV['RACK_ENV'] = 'test'
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# tell Capybara about our app class
+Capybara.app = Battle
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -100,4 +112,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
 end
